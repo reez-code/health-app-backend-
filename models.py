6a1e8f124780b8f880941d91b1c91e5bd544b34a
adan <zizkoteam@gmail.com>
2024-07-10 11:50:43 +0300

    created doctor model

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import validates
 class Doctor(db.Model,SerializerMixin):
     __tablename__ = "doctors"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)git branch
+    name = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False, unique=True)
     phone = db.Column(db.String, nullable=False)
     specialization = db.Column(db.String)
@@ -35,40 +35,40 @@ class Doctor(db.Model,SerializerMixin):
         return f"Doctor(id={self.id}, name ={self.name}, email={self.email})"
 
 
-class Appointment(db.Model,SerializerMixin):
-    __tablename__ = "appointments"
-    id = db.Column(db.Integer, primary_key=True)
-    reason=db.Column(db.String)
-    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+# class Appointment(db.Model,SerializerMixin):
+#     __tablename__ = "appointments"
+#     id = db.Column(db.Integer, primary_key=True)
+#     reason=db.Column(db.String)
+#     timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     
-    @validates("timestamp")
-    def validate_timestamp(self,key,timestamp):
-        if not isinstance(timestamp, datetime):
-            raise ValueError("Timestamp must be a datetime object")
-        return timestamp
+#     @validates("timestamp")
+#     def validate_timestamp(self,key,timestamp):
+#         if not isinstance(timestamp, datetime):
+#             raise ValueError("Timestamp must be a datetime object")
+#         return timestamp
 
-    def __repr__(self):
-        return f"Department (id={self.id}, name ={self.name}, timestamp={self.timestamp})"
+#     def __repr__(self):
+#         return f"Department (id={self.id}, name ={self.name}, timestamp={self.timestamp})"
     
     
-class Admin(db.Model, SerializerMixin):
-     __tablename__ ="admins"
-     id=db.Column(db.Integer,primary_key=True) 
-     name=db.Column(db.String, nullable=False)
-     email = db.Column(db.String, nullable=False, unique=True)
-     phone = db.Column(db.String, nullable=False)
-     role=db.Column(db.String)
+# class Admin(db.Model, SerializerMixin):
+#      __tablename__ ="admins"
+#      id=db.Column(db.Integer,primary_key=True) 
+#      name=db.Column(db.String, nullable=False)
+#      email = db.Column(db.String, nullable=False, unique=True)
+#      phone = db.Column(db.String, nullable=False)
+#      role=db.Column(db.String)
      
-     @validates('email')
-     def validate_email(self, key, email):
-        if not validators.email(email):
-            raise ValueError("Invalid email address")
-        return email
+#      @validates('email')
+#      def validate_email(self, key, email):
+#         if not validators.email(email):
+#             raise ValueError("Invalid email address")
+#         return email
 
-     @validates('phone')
-     def validate_phone(self, key, phone):
-        if not validators.length(phone, min=10, max=15):
-            raise ValueError("Phone number must be between 10 and 15 characters")
-        return phone
-     def __repr__(self):
-        return f"Admin(id={self.id}, name={self.name}, email={self.email})"
+#      @validates('phone')
+#      def validate_phone(self, key, phone):
+#         if not validators.length(phone, min=10, max=15):
+#             raise ValueError("Phone number must be between 10 and 15 characters")
+#         return phone
+    #  def __repr__(self):
+    #     return f"Admin(id={self.id}, name={self.name}, email={self.email})"
