d50b94f9ac0a6d68777455bbffddd32e4c1c9c31
lema samuel <lemasam10@gmail.com>
2024-07-10 11:36:52 +0300

    Done creating dapartment table

--- server/models.py
+++ server/models.py
@@ -1,24 +1,24 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import MetaData
-# from sqlalchemy.orm import validates
-# from sqlalchemy.ext.associationproxy import association_proxy
-# from sqlalchemy_serializer import SerializerMixin
-# import validators
-# from sqlalchemy.orm import validates
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
+import validators
+from sqlalchemy.orm import validates
 
-# metadata = MetaData(
-#     naming_convention={
-#         "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-#     }
-# )
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
-# db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy(metadata=metadata)
 
-# class Department(db.Model):
-#     __tablename__ = "departments"
+class Department(db.Model):
+    __tablename__ = "departments"
     
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
     
 
     
