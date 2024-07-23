d3e19ef76d3194a170968cd8957d34a70cf2cb27
adan <zizkoteam@gmail.com>
2024-07-23 06:14:45 +0300

    done

--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
-import validators
 from sqlalchemy.orm import validates
 from flask_bcrypt import check_password_hash
 import re
